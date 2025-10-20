33086c75e0750ac981c09a14a31f39d8f9d2c35c
Alex <petrovich.alex96@gmail.com>
2025-10-20 12:45:10 -0500

    wip

--- core/src/games/strike/Scoreboard.ts
+++ core/src/games/strike/Scoreboard.ts
@@ -1,20 +1,23 @@
-import { Entity, HDiv, NPC, Position } from "@piggo-gg/core"
+import { Entity, HDiv, HText, NPC, Player, Position } from "@piggo-gg/core"
 
 export const Scoreboard = () => {
 
   let init = false
+  const players: string[] = []
 
   const wrapper = HDiv({
     style: {
       left: "50%",
       top: "50%",
       transform: "translate(-50%, -50%)",
-      // display: "flex",
+      display: "flex",
+      flexDirection: "column",
       border: "2px solid blue",
 
       width: "400px",
       height: "300px",
-      visibility: "hidden"
+
+      // visibility: "hidden"
     }
   })
 
@@ -29,11 +32,38 @@ export const Scoreboard = () => {
             document.body.appendChild(wrapper)
           }
 
-          wrapper.style.visibility = world.client?.bufferDown.get("tab") ? "visible" : "hidden"
+          const worldPlayers = world.players()
+          for (const p of worldPlayers) {
+            if (!players.includes(p.id)) {
+              players.push(p.id)
+              const row = ScoreboardRow(p)
+              wrapper.appendChild(row)
+            }
+          }
+
+          // wrapper.style.visibility = world.client?.bufferDown.get("tab") ? "visible" : "hidden"
         }
       })
     }
   })
 
   return scoreboard
+}
+
+const ScoreboardRow = (player: Player) => {
+  const row = HDiv({
+    style: {
+      position: "relative",
+      margin: "4px",
+      width: "96%",
+      left: "50%",
+      transform: "translate(-50%)",
+      height: "24px",
+      border: "1px solid red",
+    }
+  }, HText({
+    text: player.components.pc.data.name
+  }))
+
+  return row
 }
