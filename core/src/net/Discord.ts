e15da4f992d1e605014122153b3ceb21ee2cb6d5
Alex <petrovich.alex96@gmail.com>
2025-10-31 17:54:55 -0500

    wip

--- core/src/net/Discord.ts
+++ core/src/net/Discord.ts
@@ -2,17 +2,28 @@ import { DiscordSDK } from '@discord/embedded-app-sdk'
 
 export type Discord = {
   sdk: DiscordSDK
+  userId: () => Promise<string | undefined>
 }
 
 export const Discord = (): Discord | undefined => {
+
+  let sdk = undefined
+
   try {
-    const sdk = new DiscordSDK("1433003541521236100")
-
-    console.log("Discord SDK initialized:", sdk)
-
-    return { sdk }
+    sdk = new DiscordSDK("1433003541521236100")
   } catch (error) {
     console.error("Discord SDK not available")
     return undefined
+  }
+
+  console.log("Discord SDK initialized:", sdk)
+
+  return {
+    sdk,
+    userId: async () => {
+      const user = await sdk.commands.getUser({ id: "" })
+      console.log("Discord user fetched:", user)
+      return user?.username
+    }
   }
 }
