8b88e56e44a74e826057cf582988625c819923e6
Alex <petrovich.alex96@gmail.com>
2025-11-16 18:21:21 -0600

    ship

--- core/src/ecs/systems/client/HUDSystem.ts
+++ core/src/ecs/systems/client/HUDSystem.ts
@@ -103,7 +103,7 @@ export const HUDSystem = (props: HUDSystemProps) => ClientSystemBuilder({
     return {
       id: "HUDSystem",
       query: [],
-      priority: 10,
+      priority: 0,
       onTick: () => {
         const settings = world.settings<{ showControls: boolean }>()
         wrapper.style.visibility = settings.showControls ? "visible" : "hidden"
@@ -113,26 +113,17 @@ export const HUDSystem = (props: HUDSystemProps) => ClientSystemBuilder({
         const down = client.bufferDown.all()?.map(key => key.key)
         if (down) {
           for (const btn of buttonElements) {
-            const check = btn.key === "spacebar" ? " " : btn.key
+            let check = [btn.key]
+            if (btn.key === "spacebar") check = [" "]
+            if (btn.key === "mb3") check = ["scrolldown", "scrollup"]
 
-            if (mb.includes(check)) {
-              btn.element.style.filter = down.includes(check) ? "sepia(83%) saturate(8000%) hue-rotate(170deg)" : ""
+            if (mb.includes(btn.key)) {
+              btn.element.style.filter = check.some(x => down.includes(x)) ? "sepia(83%) saturate(8000%) hue-rotate(170deg)" : ""
             } else {
-              btn.element.style.backgroundColor = down.includes(check) ? active : inactive
+              btn.element.style.backgroundColor = check.some(x => down.includes(x)) ? active : inactive
             }
           }
         }
-
-        // const state = world.state<{ phase: string }>()
-
-        // const isConnected = world.client?.net.synced
-        // const isWarmup = state.phase === "warmup"
-
-        // cButton.style.visibility = isWarmup ? "visible" : "hidden"
-        // teamLabel.style.visibility = isWarmup ? "visible" : "hidden"
-
-        // zButton.style.visibility = isWarmup && isConnected ? "visible" : "hidden"
-        // readyLabel.style.visibility = isWarmup && isConnected ? "visible" : "hidden"
       }
     }
   }
