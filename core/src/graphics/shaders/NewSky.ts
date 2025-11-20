43d3fdb0572713b5b9d5fdf9ad5ab2320369858a
Alex <petrovich.alex96@gmail.com>
2025-11-20 10:59:33 -0600

    wip

--- core/src/graphics/shaders/NewSky.ts
+++ core/src/graphics/shaders/NewSky.ts
@@ -61,59 +61,52 @@ const fragmentShader = /* glsl */`
   uniform vec2 uResolution;
   varying vec3 vWorldPosition;
 
-  #define iMouse vec2(0.5, 0.5)
+  #define iMouse vec2(500, 500)
   #define PI 3.14159265359
 
   vec3 getSky(vec2 uv) {
     float atmosphere = sqrt(1.0-uv.y);
-    vec3 skyColor = vec3(0.2,0.4,0.8);
+    vec3 skyColor = vec3(0.2, 0.4, 0.8);
     
     float scatter = pow(iMouse.y / uResolution.y, 1.0 / 15.0);
-    scatter = 1.0 - clamp(scatter,0.8,1.0);
-    
-    vec3 scatterColor = mix(vec3(1.0),vec3(1.0,0.3,0.0) * 1.5,scatter);
-    return mix(skyColor,vec3(scatterColor),atmosphere / 1.3);
+    scatter = 1.0 - clamp(scatter, 0.8, 1.0);
+
+    vec3 scatterColor = mix(vec3(1.0), vec3(1.0, 0.3, 0.0) * 1.5, scatter);
+    return mix(skyColor, vec3(scatterColor), atmosphere / 1.3);
   }
 
   vec3 getSun(vec2 uv) {
-  float sun = 1.0 - distance(uv,iMouse.xy / uResolution.y);
-    sun = clamp(sun,0.0,1.0);
-    
+    float sun = 1.0 - distance(uv,iMouse.xy / uResolution.y);
+    sun = clamp(sun, 0.0, 1.0);
+
     float glow = sun;
-    glow = clamp(glow,0.0,1.0);
-    
-    sun = pow(sun,100.0);
+    glow = clamp(glow, 0.0, 1.0);
+
+    sun = pow(sun, 100.0);
     sun *= 100.0;
-    sun = clamp(sun,0.0,1.0);
-    
-    glow = pow(glow,6.0) * 1.0;
-    glow = pow(glow,(uv.y));
-    glow = clamp(glow,0.0,1.0);
-    
+    sun = clamp(sun, 0.0, 1.0);
+
+    glow = pow(glow, 6.0) * 1.0;
+    glow = pow(glow, (uv.y));
+    glow = clamp(glow, 0.0, 1.0);
+
     sun *= pow(dot(uv.y, uv.y), 1.0 / 1.65);
-    
+
     glow *= pow(dot(uv.y, uv.y), 1.0 / 2.0);
-    
+
     sun += glow;
-    
-    vec3 sunColor = vec3(1.0,0.6,0.05) * sun;
-    
+
+    vec3 sunColor = vec3(1.0, 0.6, 0.05) * sun;
+
     return vec3(sunColor);
   }
 
   void main() {
-    // vec2 uv = fragCoord.xy / uResolution.y;
-
-    // vec3 dir = normalize(vWorldPosition - cameraPosition);
-
-    // float u = atan(dir.x, dir.z) / (2.0 * PI) + 0.5;
-    // float v = dir.y * 0.5 + 0.5;
-    // vec2 uv = vec2(u, v);
     vec2 uv = gl_FragCoord.xy / uResolution;
-      
+
     vec3 sky = getSky(uv);
     vec3 sun = getSun(uv);
-      
-    gl_FragColor = vec4(sky + sun,1.0);
+
+    gl_FragColor = vec4(sky + sun, 1.0);
   }
 `
