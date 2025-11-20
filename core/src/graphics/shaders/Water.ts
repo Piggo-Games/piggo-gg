5e36005d104c81962e9bcffa7eebf238e226de0c
Alex <petrovich.alex96@gmail.com>
2025-11-20 13:19:14 -0600

    wip

--- core/src/graphics/shaders/Water.ts
+++ core/src/graphics/shaders/Water.ts
@@ -1,13 +1,9 @@
 import { Entity, Position, Three } from "@piggo-gg/core"
-import {
-  Color, Mesh, SphereGeometry, ShaderMaterial, PlaneGeometry,
-  TextureLoader, RepeatWrapping, Vector3
-} from "three"
+import { PlaneGeometry, TextureLoader, RepeatWrapping, Vector3 } from "three"
 import { Water as ThreeWater } from "three/examples/jsm/objects/Water.js"
 
 export const Water = () => {
 
-  let mat: ShaderMaterial | undefined = undefined
   let water: ThreeWater | undefined = undefined
 
   const sky = Entity<Three>({
@@ -24,15 +20,15 @@ export const Water = () => {
           const waterGeo = new PlaneGeometry(10000, 10000)
 
           water = new ThreeWater(waterGeo, {
-            textureWidth: 1024,
-            textureHeight: 1024,
+            textureWidth: 512,
+            textureHeight: 512,
             waterNormals: new TextureLoader().load('waternormals.jpg', function (texture) {
               texture.wrapS = RepeatWrapping
               texture.wrapT = RepeatWrapping
             }),
-            sunDirection: new Vector3(1, 0.2, 1),
-            sunColor: 0x000000,
-            waterColor: 0x001e9f,
+            sunDirection: new Vector3(2, 0.4, 1),
+            sunColor: 0xffffff,
+            waterColor: 0x1122a0,
             distortionScale: 2,
             clipBias: 0.003,
             side: 2
