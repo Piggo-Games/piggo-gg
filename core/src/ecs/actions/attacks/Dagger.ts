b892a9509ff9c582f237e9e7b41c8c45e7410d68
Alex <petrovich.alex96@gmail.com>
2025-11-25 07:35:58 -0600

    wip

--- core/src/ecs/actions/attacks/Dagger.ts
+++ core/src/ecs/actions/attacks/Dagger.ts
@@ -1,5 +1,29 @@
-import { Character } from "@piggo-gg/core"
+import { Action, Actions, Character, Effects, Entity, Input, Item, ItemComponents, Networked, Position } from "@piggo-gg/core"
 
 export const DaggerItem = ({ character }: { character: Character }) => {
 
+
+  const item = Entity<ItemComponents>({
+    id: `dagger-${character.id}`,
+    components: {
+      position: Position(),
+      effects: Effects(),
+      networked: Networked(),
+      item: Item({ name: "dagger", stackable: false }),
+      input: Input({
+        press: {
+          "mb1": ({ character, world, aim, client }) => {
+            console.log("Dagger MB1")
+          }
+        }
+      }),
+      actions: Actions({
+        swing: Action("swing", ({ world, params }) => {
+
+        })
+      })
+    }
+  })
+
+  return item
 }
