6444f6edb46d3917b1f6c5835403b95f75ddd411
Alex <petrovich.alex96@gmail.com>
2025-11-25 18:15:24 -0600

    wip

--- core/src/ecs/actions/attacks/Dagger.ts
+++ core/src/ecs/actions/attacks/Dagger.ts
@@ -1,9 +1,12 @@
-import { Action, Actions, Character, Effects, Entity, Input, Item, ItemComponents, modelOffset, Networked, PI, Position, Three } from "@piggo-gg/core"
+import { Action, Actions, Character, Effects, Entity, Input, Item, ItemComponents, max, modelOffset, Networked, NPC, PI, Position, Three } from "@piggo-gg/core"
 import { Mesh, Object3D } from "three"
 
 export const DaggerItem = ({ character }: { character: Character }) => {
 
   let mesh: Object3D | undefined = undefined
+  let cd = -100
+
+  const recoilRate = 0.06
 
   const item = Entity<ItemComponents>({
     id: `dagger-${character.id}`,
@@ -12,16 +15,31 @@ export const DaggerItem = ({ character }: { character: Character }) => {
       effects: Effects(),
       networked: Networked(),
       item: Item({ name: "dagger", stackable: false }),
+      npc: NPC({
+        behavior: () => {
+          const { recoil } = character.components.position.data
+
+          if (recoil > 0) {
+            character.components.position.data.recoil = max(0, recoil - recoilRate)
+          }
+        }
+      }),
       input: Input({
         press: {
           "mb1": ({ character, world, aim, client }) => {
-            console.log("Dagger MB1")
+            if (!character) return
+            if (!document.pointerLockElement && !client.mobile) return
+
+            if (cd + 14 > world.tick) return
+            cd = world.tick
+
+            return { actionId: "swing", params: {} }
           }
         }
       }),
       actions: Actions({
         swing: Action("swing", ({ world, params }) => {
-
+          character.components.position.data.recoil = 0.5
         })
       }),
       three: Three({
@@ -41,12 +59,12 @@ export const DaggerItem = ({ character }: { character: Character }) => {
 
             mesh.rotation.order = "YXZ"
 
-            console.log("Dagger mesh loaded", mesh)
-
             o.push(mesh)
           })
         },
         onRender: ({ world, delta, client, three }) => {
+          const ratio = delta / 25
+
           const pos = character.components.position.interpolate(world, delta)
 
           let { aim } = character.components.position.data
@@ -56,12 +74,12 @@ export const DaggerItem = ({ character }: { character: Character }) => {
 
           if (!mesh) return
 
-          // if (three.camera.mode === "third" && character.id === world.client?.character()?.id) {
-          //   mesh.visible = false
-          //   return
-          // } else {
-          //   mesh.visible = true
-          // }
+          if (three.camera.mode === "third" && character.id === world.client?.character()?.id) {
+            mesh.visible = false
+            return
+          } else {
+            mesh.visible = true
+          }
 
           if (character.components.health?.dead()) {
             mesh.visible = false
@@ -76,11 +94,11 @@ export const DaggerItem = ({ character }: { character: Character }) => {
             pos.y + offset.z
           )
 
-          // const { recoil } = character.components.position.data
-          // const localRecoil = recoil ? recoil - recoilRate * ratio : 0
+          const { recoil } = character.components.position.data
+          const localRecoil = recoil ? recoil - recoilRate * ratio : 0
 
           mesh.rotation.y = aim.x + PI / 2
-          mesh.rotation.z = aim.y //+ localRecoil * 0.5
+          mesh.rotation.z = aim.y - localRecoil * 0.5
         }
       })
     }
