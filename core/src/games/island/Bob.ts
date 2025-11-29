8c4add387693cbee45c93a37a00d4cdc8b485b5b
Alex <petrovich.alex96@gmail.com>
2025-11-28 21:11:00 -0500

    wip

--- core/src/games/island/Bob.ts
+++ core/src/games/island/Bob.ts
@@ -505,7 +505,7 @@ export const Bob = (player: Player): Character => {
             block.material.forEach((mat) => mat.color.set(blockColor))
           }
         },
-        init: async ({ o, entity, world, three }) => {
+        init: async ({ o, world, three }) => {
 
           // body
           const bodyGeo = new CapsuleGeometry(0.064, 0.34)
