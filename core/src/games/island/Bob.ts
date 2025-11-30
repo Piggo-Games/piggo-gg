876c25229d3d7fdd425c3fb365bd9a4b4fb1e4ad
Alex <petrovich.alex96@gmail.com>
2025-11-30 11:12:39 -0500

    wip

--- core/src/games/island/Bob.ts
+++ core/src/games/island/Bob.ts
@@ -4,8 +4,7 @@ import {
   Point, Position, Team, XYZ, XZ, IslandSettings, cloneSkeleton,
   Ready, ColorMapping, nextColor, MarbleTexture, BlockMaterial,
   IslandState, blockInLine, hypot, Input, Three, upAndDir, colorMaterials,
-  cos, sin, BlocksMesh, nextBlock, DaggerItem, setActiveItemIndex,
-  Debug
+  cos, sin, BlocksMesh, nextBlock, DaggerItem, setActiveItemIndex
 } from "@piggo-gg/core"
 import {
   AnimationAction, AnimationMixer, BoxGeometry, CapsuleGeometry, Mesh, MeshPhongMaterial,
