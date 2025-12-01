ba37e6649984bf472fe1eb6ebd4229b1799c441a
Alex <petrovich.alex96@gmail.com>
2025-11-30 21:38:59 -0500

    wip

--- core/src/games/island/Shork.ts
+++ core/src/games/island/Shork.ts
@@ -1,11 +1,9 @@
 import { Collider, Entity, NPC, Position, Three } from "@piggo-gg/core"
-import { BoxGeometry, Group, Mesh, MeshPhongMaterial, Object3DEventMap } from "three"
+import { Group, Mesh, Object3DEventMap } from "three"
 
 export const Shork = () => {
 
   let mesh: Group<Object3DEventMap> | undefined = undefined
-  let died = false
-  let hitboxes: { body?: Mesh } = {}
 
   const shork = Entity<Position>({
     id: "shork",
@@ -14,26 +12,9 @@ export const Shork = () => {
       collider: Collider({ shape: "ball", radius: 0.1 }),
       npc: NPC({
         behavior: (_, world) => {
-          if (!mesh || died) return
+          if (!mesh) return
 
           const pc = world.client?.character()
-          // if (!died && pc?.components.inventory?.activeItem(world)?.id.startsWith("dagger")) {
-          //   died = true
-          //   mesh.rotation.x = Math.PI / 2
-          //   hitboxes.body!.visible = false
-          // }
-
-          // mesh.rotation.y += world.debug ? 0.01 : 0
-
-          // move hitbox
-          // if (hitboxes.body) {
-          //   const pos = shork.components.position.data
-          //   hitboxes.body.position.set(pos.x, pos.z + 0.17, pos.y)
-          //   hitboxes.body.rotation.y = mesh.rotation.y
-
-          //   hitboxes.body.visible = world.debug
-          // }
-
 
           // if swimming, move toward player
           if (pc?.components.position.data.swimming) {
@@ -70,12 +51,6 @@ export const Shork = () => {
           }
         },
         init: async ({ o, three }) => {
-          const bodyGeo = new BoxGeometry(0.28, 0.18, 0.34)
-          const bodyMat = new MeshPhongMaterial({ color: 0x0000ff, wireframe: true })
-          hitboxes.body = new Mesh(bodyGeo, bodyMat)
-
-          o.push(hitboxes.body)
-
           three.gLoader.load("shork.gltf", (gltf) => {
             mesh = gltf.scene
 
