d2c00f985e9d76b1c8d21826a793f4384ce8ed41
Alex <petrovich.alex96@gmail.com>
2025-12-07 18:14:22 -0600

    wip

--- core/src/games/gamba/enemies/BlueGuy.ts
+++ core/src/games/gamba/enemies/BlueGuy.ts
@@ -1,14 +1,18 @@
-import { DudeSkin, Entity, Position, Renderable, VolleyCharacterAnimations, VolleyCharacterDynamic } from "@piggo-gg/core"
+import { Collider, Debug, DudeSkin, Entity, Position, Renderable, VolleyCharacterAnimations, VolleyCharacterDynamic } from "@piggo-gg/core"
 
 export const BlueGuy = (): Entity => {
 
   const blueGuy = Entity({
     id: "blue-guy",
     components: {
-      position: Position(),
+      position: Position({ x: 120 }),
+      debug: Debug(),
+      collider: Collider ({
+        shape: "ball", radius: 5, isStatic: true, group: "all"
+      }),
       renderable: Renderable({
         anchor: { x: 0.55, y: 0.9 },
-        scale: 1.2,
+        scale: -1.2,
         zIndex: 4,
         interpolate: true,
         scaleMode: "nearest",
