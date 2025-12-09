d67ea7fbb4a7ebadbcb908d5b579b1a311e60be7
Alex <petrovich.alex96@gmail.com>
2025-12-09 13:04:11 -0600

    ship

--- core/src/games/gamba/ui/NumBoard.ts
+++ core/src/games/gamba/ui/NumBoard.ts
@@ -28,7 +28,7 @@ export const NumBoard = (): Entity => {
       const sprite = digits[key]
       console.log("showing", key, sprite)
 
-      sprite.x = startX + i * 11 * 3
+      sprite.x = startX + i * 11 * 3 - 16
       sprite.y = 0
       sprite.visible = true
     }
@@ -37,10 +37,11 @@ export const NumBoard = (): Entity => {
   const numBoard = Entity({
     id: "num-board",
     components: {
-      position: Position({ x: 0, y: -140 }),
+      position: Position({ x: 0, y: -83 }),
       renderable: Renderable({
-        zIndex: 5,
+        zIndex: 2,
         scaleMode: "nearest",
+        anchor: { x: 0.5, y: 0.5 },
         onTick: ({ world }) => {
           const state = world.state<GambaState>()
           if (state.rolled !== null && state.rolled !== rendered) {
@@ -49,6 +50,18 @@ export const NumBoard = (): Entity => {
           }
         },
         setup: async (r) => {
+          const board = await loadTexture("BoardGreen.json")
+          const left = new Sprite(board["0"])
+          const right = new Sprite(board["1"])
+          left.anchor.set(0.5, 0.5)
+          right.anchor.set(0.5, 0.5)
+          left.texture.source.scaleMode = "nearest"
+          right.texture.source.scaleMode = "nearest"
+          left.x = -26.5
+          right.x = 26.5
+          r.c.addChild(left)
+          r.c.addChild(right)
+
           const nums = await loadTexture("nums.json")
 
           for (let i of [0, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9]) {
