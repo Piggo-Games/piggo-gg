826dadc1decff0cb5537e80833c0f425f7a3a657
Alex <petrovich.alex96@gmail.com>
2025-12-27 09:36:35 -0500

    wip

--- core/src/games/mars/entities/Launchpad.ts
+++ core/src/games/mars/entities/Launchpad.ts
@@ -1,4 +1,5 @@
-import { Entity, pixiGraphics, Position, Renderable } from "@piggo-gg/core"
+import { Entity, load, pixiGraphics, Position, Renderable } from "@piggo-gg/core"
+import { Sprite } from "pixi.js/lib"
 
 export const Launchpad = (): Entity => {
   return Entity({
@@ -7,18 +8,28 @@ export const Launchpad = (): Entity => {
       position: Position({ x: 0, y: 140 }),
       renderable: Renderable({
         zIndex: 3,
-        scale: 0.3,
         setup: async (r) => {
           const g = pixiGraphics()
 
-          g.moveTo(-100, -50)
-            .lineTo(100, -50)
-            .lineTo(130, 50)
-            .lineTo(-130, 50)
-            .lineTo(-100, -50)
+          const w = 40
+          const h = 26
+
+          g.moveTo(-w, -h)
+            .lineTo(w, -h)
+            .lineTo(w + 30, h)
+            .lineTo(-w - 30, h)
+            .lineTo(-w, -h)
             .fill({ color: 0x555555 })
 
-          r.c = g
+          r.c.addChild(g)
+
+          const rail = await load("rail.png")
+          rail.source.scaleMode = "nearest"
+          const railSprite = new Sprite({ texture: rail, scale: 2 })
+          railSprite.anchor.set(0, 1)
+          railSprite.position.set(-20, 0)
+
+          r.c.addChild(railSprite)
         }
       })
     }
