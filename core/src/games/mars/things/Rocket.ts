33c8cb78fa93049f0332002c3b0cfba9a88e9dd5
Alex <petrovich.alex96@gmail.com>
2025-12-31 17:16:30 -0600

    wip

--- core/src/games/mars/things/Rocket.ts
+++ core/src/games/mars/things/Rocket.ts
@@ -1,10 +1,14 @@
 import { Collider, Debug, Entity, load, MarsState, NPC, Position, Renderable, Shadow } from "@piggo-gg/core"
 import { Sprite } from "pixi.js"
+import { Emitter } from "@sosuisen/particle-emitter"
 
 const speed = 20
 const maxZ = 3500
 
 export const Rocket = (): Entity => {
+
+  let emitter: Emitter | undefined
+
   const rocket = Entity<Position | Renderable>({
     id: "rocket",
     components: {
@@ -34,10 +38,125 @@ export const Rocket = (): Entity => {
         zIndex: 5,
         scale: 0.3,
         interpolate: true,
+        onRender: () => {
+          if (!emitter) return
+          console.log("emitting")
+          emitter.update(1 / 60)
+          emitter.emit = true
+        },
         setup: async (r, renderer) => {
           const f9 = await load("flamin-9.png")
 
           r.c = new Sprite(f9)
+
+          emitter = new Emitter(r.c, {
+            emit: true,
+            lifetime: {
+              min: 0.1,
+              max: 0.75
+            },
+            frequency: 0.001,
+            emitterLifetime: 0,
+            maxParticles: 1000,
+            addAtBack: false,
+            pos: {
+              x: 0,
+              y: 0
+            },
+            behaviors: [
+              {
+                type: "alpha",
+                config: {
+                  alpha: {
+                    list: [
+                      {
+                        time: 0,
+                        value: 0.62
+                      },
+                      {
+                        time: 1,
+                        value: 0
+                      }
+                    ]
+                  }
+                }
+              },
+              {
+                type: "moveSpeedStatic",
+                config: {
+                  min: 500,
+                  max: 500
+                }
+              },
+              {
+                type: "scale",
+                config: {
+                  scale: {
+                    list: [
+                      {
+                        time: 0,
+                        value: 0.25
+                      },
+                      {
+                        time: 1,
+                        value: 0.75
+                      }
+                    ]
+                  },
+                  minMult: 1
+                }
+              },
+              {
+                type: "color",
+                config: {
+                  color: {
+                    list: [
+                      {
+                        time: 0,
+                        value: "fff191"
+                      },
+                      {
+                        time: 1,
+                        value: "ff622c"
+                      }
+                    ]
+                  }
+                }
+              },
+              {
+                type: "rotation",
+                config: {
+                  accel: 0,
+                  minSpeed: 50,
+                  maxSpeed: 50,
+                  minStart: 265,
+                  maxStart: 275
+                }
+              },
+              {
+                type: "textureRandom",
+                config: {
+                  textures: [
+                    "particle.png",
+                    "fire.png"
+                  ]
+                }
+              },
+              {
+                type: "spawnShape",
+                config: {
+                  type: "torus",
+                  data: {
+                    x: 0,
+                    y: 0,
+                    radius: 10,
+                    innerRadius: 0,
+                    affectRotation: false
+                  }
+                }
+              }
+            ]
+          })
 
           renderer.camera.focus = rocket
         }
