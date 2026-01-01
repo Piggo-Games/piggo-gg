93ac7b480c7d5720eacfb0097b183ddd8f4805a5
Alex <petrovich.alex96@gmail.com>
2026-01-01 04:41:59 -0600

    wip

--- core/src/utils/SystemUtils.ts
+++ core/src/utils/SystemUtils.ts
@@ -1,4 +1,4 @@
-export const piggoVersion: `0.${number}.${number}` = "0.55.1"
+export const piggoVersion: `0.${number}.${number}` = "0.55.2"
 
 export const isMobile = (): boolean => /iPhone|iPad|iPod|Android/i.test(navigator.userAgent)
 
